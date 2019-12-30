443c6327fd2132d493ac668bf3dce649c054e651
Doak Covington <doakcovington@protonmail.com>
2019-12-30 03:03:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -32,7 +32,7 @@ def assign_rooms(list)
 end
 
 def printer(name)
-  batch_badge_creator(array).each do |name|
-    puts name
+  batch_badge_creator(name).each do |attendee|
+    puts attendee
   end
 end
