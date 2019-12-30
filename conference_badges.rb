a07bd56ea01652345aeeb34b9175cb3c5ce72d80
Doak Covington <doakcovington@protonmail.com>
2019-12-30 02:26:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,7 @@ def assign_rooms(list)
   room_assignments = []
 
   list.each do |name|
-    room_assignments << "Hello, #{name}! You'll be assigned to room #{list.each_with_index}! "
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{0 ++}! "
   end
 
   return room_assignments
